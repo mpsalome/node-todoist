3ce98326b19d10b3e13426a1e362ceec605c93b1
Marcus Salomé <marcuspedro7@gmail.com>
2022-05-16 20:45:19 -0300

    Added responses on the todoController

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,19 +1,25 @@
 exports.get = (req, res) => {
+  const id = req.params.id;
   console.log('Get');
+  res.send(`OK GET id=${id}`);
 };
 
 exports.getAll = (req, res) => {
   console.log('Get All');
+  res.send('OK GET All');
 };
 
 exports.add = (req, res) => {
   console.log('Add');
+  res.send('OK Add');
 };
 
 exports.update = (req, res) => {
   console.log('Update');
+  res.send('OK Update');
 };
 
 exports.delete = (req, res) => {
   console.log('Delete');
+  res.send('OK Delete');
 };
