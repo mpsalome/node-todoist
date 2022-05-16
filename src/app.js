b6b0370263adb4d509b36e182b9805484467bf43
Marcus Salomé <marcuspedro7@gmail.com>
2022-05-16 20:21:39 -0300

    Setted up routers and todoController

--- src/app.js
+++ src/app.js
@@ -4,4 +4,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const indexRoute = require('./routers/index');
+const indexTodo = require('./routers/index');
+
+app.use('/', indexRoute);
+app.use('/todos', indexTodo);
+
 module.exports = app;
---
+++ src/controllers/todoController.js
@@ -0,0 +1,19 @@
+exports.get = (req, res) => {
+  console.log('Get');
+};
+
+exports.getAll = (req, res) => {
+  console.log('Get All');
+};
+
+exports.add = (req, res) => {
+  console.log('Add');
+};
+
+exports.update = (req, res) => {
+  console.log('Update');
+};
+
+exports.delete = (req, res) => {
+  console.log('Delete');
+};
